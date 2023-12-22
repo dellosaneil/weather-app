9a5d73fcf242fac779b19ce7333cf253b8da5488
neiljedricdellosa <neil.d@poplarhomes.com>
2023-12-22 11:28:15 +0800

    added cache

--- data/src/main/java/com/thelazybattley/data/di/NetworkModule.kt
+++ data/src/main/java/com/thelazybattley/data/di/NetworkModule.kt
@@ -1,15 +1,19 @@
 package com.thelazybattley.data.di
 
+import android.content.Context
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import com.thelazybattley.data.network.service.OpenMateoService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.time.Duration
 import javax.inject.Singleton
 
@@ -19,7 +23,12 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(
+        @ApplicationContext context: Context
+    ): Retrofit {
+        val cacheSize = 10 * 1024 * 1024L
+        val file = File(context.cacheDir, "http-cache")
+        val cache = Cache(file, cacheSize)
         val gson = GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
             .create()
@@ -27,6 +36,7 @@ object NetworkModule {
         val httpClient = OkHttpClient.Builder()
             .connectTimeout(Duration.ofSeconds(15))
             .readTimeout(Duration.ofSeconds(15))
+            .cache(cache = cache)
             .build()
 
         return Retrofit.Builder()
